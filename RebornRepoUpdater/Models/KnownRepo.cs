7485efa5b26dd263caa3d9561e914561ec7cdb37
Thomas Brooks <34015422+NostraThomas99@users.noreply.github.com>
2024-12-05 21:27:19 -0600

    dotnet conversion

---
+++ RebornRepoUpdater/Models/KnownRepo.cs
@@ -0,0 +1,125 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+using Octokit;
+using RebornRepoUpdater.Models.Dalamud;
+
+namespace RebornRepoUpdater.Models;
+
+public class KnownRepo
+{
+    public string ProjectName { get; set; }
+    public string OrganizationName { get; set; }
+    public string ManifestFilePath { get; set; }
+    public string InternalName { get; set; }
+    public PluginManifest? Manifest { get; private set; } = null;
+
+    public async Task Update(GitHubClient client)
+    {
+        Console.WriteLine($"Getting current manifest for {ProjectName}...");
+
+        await EnsureRateLimitAsync(client);
+
+        var jsonContent =
+            await client.Repository.Content.GetAllContents(ProjectName, OrganizationName, ManifestFilePath);
+        var json = jsonContent.First().Content;
+        var manifest = JsonConvert.DeserializeObject<PluginManifest>(json);
+        Manifest = manifest;
+
+        if (Manifest != null)
+            await GetReleaseInfo(client);
+    }
+
+    private async Task GetReleaseInfo(GitHubClient client)
+    {
+        Console.WriteLine($"Getting releases for {ProjectName}...");
+
+        await EnsureRateLimitAsync(client);
+
+        var releases = await client.Repository.Release.GetAll(OrganizationName, ProjectName);
+        var releasesOrderedByDate = releases.OrderByDescending(r => r.PublishedAt);
+
+        var latestRelease = releasesOrderedByDate.FirstOrDefault(r => !r.Prerelease); // Latest stable release
+        var latestTestingRelease = releasesOrderedByDate.FirstOrDefault(r => r.Prerelease); // Latest prerelease
+
+        try
+        {
+            if (latestRelease != null)
+            {
+                Console.WriteLine($"Latest stable release: {latestRelease.Name}");
+                Manifest.AssemblyVersion = latestRelease.TagName;
+                Manifest.DownloadLinkInstall =
+                    latestRelease.Assets.First(a => a.Name.EndsWith(".zip")).BrowserDownloadUrl;
+                Manifest.Changelog = latestRelease.Body; // Release notes or changelog
+            }
+            else
+            {
+                Console.WriteLine("No stable release found.");
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"Failed to get releases for {ProjectName}", ex);
+        }
+
+        try
+        {
+            if (latestTestingRelease != null)
+            {
+                Console.WriteLine($"Latest testing release: {latestTestingRelease.Name}");
+                Manifest.TestingAssemblyVersion = latestTestingRelease.TagName;
+                Manifest.DownloadLinkTesting =
+                    latestTestingRelease.Assets.First(a => a.Name.EndsWith(".zip")).BrowserDownloadUrl;
+            }
+            else
+            {
+                Console.WriteLine("No testing release found.");
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"Failed to get testing releases for {ProjectName}", ex);
+        }
+
+        Manifest.SetDownloadCount(releasesOrderedByDate);
+        Console.WriteLine($"Download count: {Manifest.DownloadCount}");
+    }
+
+    private async Task EnsureRateLimitAsync(GitHubClient client)
+    {
+        var apiInfo = await client.RateLimit.GetRateLimits();
+        var rateLimit = apiInfo?.Rate;
+
+        if (rateLimit == null)
+        {
+            Console.WriteLine("No rate limit information available. Proceeding with API call.");
+            return;
+        }
+
+        var remaining = rateLimit.Remaining;
+        var resetTime = rateLimit.Reset;
+
+        if (remaining > 0)
+        {
+            Console.WriteLine($"Rate Limit OK: {remaining} requests remaining.");
+            return;
+        }
+
+        var waitTime = resetTime - DateTimeOffset.UtcNow;
+        if (waitTime.TotalSeconds > 0)
+        {
+            Console.WriteLine($"Rate limit reached. Waiting for {waitTime.TotalSeconds:N0} seconds...");
+            await Task.Delay(waitTime);
+        }
+    }
+
+
+
+    public KnownRepo(string projectName, string internalName, string organizationName = "FFXIV-CombatReborn",
+        string manifestFilePath = "")
+    {
+        ProjectName = projectName;
+        OrganizationName = organizationName;
+        InternalName = internalName;
+        ManifestFilePath = string.IsNullOrWhiteSpace(manifestFilePath) ? $"manifest.json" : manifestFilePath;
+    }
+}
---
+++ RebornRepoUpdater/Program.cs
@@ -0,0 +1,95 @@
+﻿using System.Net;
+using Newtonsoft.Json;
+using Octokit;
+using RebornRepoUpdater.Models;
+using RebornRepoUpdater.Models.Dalamud;
+
+namespace RebornRepoUpdater;
+
+class Program
+{
+    public static GitHubClient Client = new GitHubClient(new ProductHeaderValue("CombatRebornRepoUpdater"));
+
+    public static List<KnownRepo> KnownRepos =
+    [
+        new KnownRepo("RotationSolverReborn", "RotationSolver"),
+        new KnownRepo("BossModReborn", "BossModReborn"),
+        new KnownRepo("ActionTimelineReborn", "ActionTimelineEx"),
+        new KnownRepo("EasyZoomReborn", "EasyZoomReborn"),
+        new KnownRepo("GatherBuddyReborn", "GatherbuddyReborn"),
+        new KnownRepo("RebornToolbox", "RebornToolbox"),
+    ];
+
+    static async Task Main(string[] args)
+    {
+        string? filePath = null;
+        string? githubToken = null;
+
+        for (int i = 0; i < args.Length; i++)
+        {
+            switch (args[i])
+            {
+                case "--path":
+                    filePath = args[i + 1];
+                    i++;
+                    break;
+                case "--token":
+                    githubToken = args[i + 1];
+                    i++;
+                    break;
+            }
+        }
+
+        if (filePath == null)
+        {
+            Console.WriteLine("File path is required.");
+            return;
+        }
+
+        if (githubToken == null)
+        {
+            Client.Credentials = Credentials.Anonymous;
+        }
+        else
+        {
+            Client.Credentials = new Credentials(githubToken);
+        }
+
+        var manifestJson = File.ReadAllText(filePath);
+        var manifests = JsonConvert.DeserializeObject<List<PluginManifest>>(manifestJson) ?? new List<PluginManifest>();
+        var updatedManifests = new List<PluginManifest>();
+
+        foreach (var repo in KnownRepos)
+        {
+            Console.WriteLine($"Processing {repo.ProjectName}");
+            var localManifest = manifests.FirstOrDefault(m => m.InternalName == repo.InternalName);
+
+            try
+            {
+                await repo.Update(Client);
+
+                if (repo.Manifest == null)
+                    throw new Exception("Manifest was still null after updating.");
+                else
+                    updatedManifests.Add(repo.Manifest);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to update manifest for {repo.ProjectName}: {ex.Message}");
+                if (localManifest != null)
+                {
+                    Console.WriteLine($"Falling back to local manifest for {repo.ProjectName}");
+                    updatedManifests.Add(localManifest);
+                }
+                else
+                {
+                    Console.WriteLine($"Local manifest for {repo.ProjectName} does not exist, PROJECT WILL NOT BE PART OF PLUGINMASTER DATA.");
+                }
+            }
+        }
+
+        Console.WriteLine($"Found {updatedManifests.Count} manifests from both remote and local sources");
+        var manifestsJson = JsonConvert.SerializeObject(updatedManifests, Formatting.Indented);
+        await File.WriteAllTextAsync(filePath, manifestsJson);
+    }
+}
