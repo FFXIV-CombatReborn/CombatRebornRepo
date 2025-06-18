0e2ca3679f611af5b24fe52af0b2eb09165f1210
LTS <127939494+LTS-FFXIV@users.noreply.github.com>
2025-06-18 11:14:49 -0500

    Add RepoUrl

--- RebornRepoUpdater/Models/Dalamud/PluginManifest.cs
+++ RebornRepoUpdater/Models/Dalamud/PluginManifest.cs
@@ -11,6 +11,8 @@ public class PluginManifest
 
     [JsonProperty("InternalName")] public string InternalName { get; set; }
 
+    [JsonProperty("RepoUrl")] public string RepoUrl { get; set; }
+
     [JsonProperty("Description")] public string Description { get; set; }
 
     [JsonProperty("ApplicableVersion")] public string ApplicableVersion { get; set; }
